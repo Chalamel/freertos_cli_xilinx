e15a8096c04e1bf1f63f7be4f3610a35aec2fe6b
Karl Emil Bohne <karlemil.sb@gmail.com>
2019-09-04 10:32:04 +0200

    Make CLI generic. Define CLI - device I/F

--- src/embedded/cli.c
+++ src/embedded/cli.c
@@ -1,125 +1,182 @@
 /*
  * @file cli.c
  * 
- * @brief Simple FreeRTOS CLI implementation providing AXI address-space access
+ * Simple FreeRTOS CLI implementation providing address-space access
  *
  * @author Karl Emil Sandvik Bohne
  */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-#include "sw_opts.h"
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <FreeRTOS_CLI.h>
 
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
+#include <cli.h>
 
-#include "xil_types.h"
-#include "xil_io.h"
-
-#include "cli.h"
-
-#include YOUR_UART_OR_SOCKET_HEADER
-
-
-#define MAX_INPUT_LENGTH 200 //<! Max allowable length of terminal input
-#define MAX_OUTPUT_LENGTH 200 //<! Max allowable length of terminal output (per call to command handler)
-#define UART_BUF_SIZE 1024
-
-QueueHandle_t uart_buf; //<! The UART - CLI queue. Change this to a StreamBuffer when it works
+#ifndef configCOMMAND_INT_MAX_OUTPUT_SIZE
+#define configCOMMAND_INT_MAX_OUTPUT_SIZE MAX_OUTPUT_LENGTH
+#endif
 
 /**
- * @brief Registers the defined commands with the CLI, allowing them to be used.
+ * As defined in FreeRTOSConfig.h
  */
-BaseType_t reg_cli_cmds() {
+#if configAPPLICATION_PROVIDES_cOutputBuffer == 1
+#define configCOMMAND_INT_MAX_OUTPUT_SIZE MAX_OUTPUT_LENGTH * 2
+char cli_tx_buf[configCOMMAND_INT_MAX_OUTPUT_SIZE];
+#endif
 
-    if (FreeRTOS_CLIRegisterCommand(rd_addr_cmd_ptr) != pdPASS) {
-        xil_printf("Failed to the add read-command for the CLI.\n");
+
+static BaseType_t reg_cli_cmds(void);
+static BaseType_t cli_rd_cmd_handler(char *wr_buf, size_t wr_buf_len, const char *cmd_str);
+static BaseType_t cli_wr_cmd_handler(char *wr_buf, size_t wr_buf_len, const char *cmd_str);
+
+
+/**
+ * Defines the read-address command by its signature, description, callback function pointer,
+ * and number of parameters
+ */
+static const CLI_Command_Definition_t rd_addr_cmd = {
+    "rd",
+    "rd <addr>: Returns the value stored at <addr>\r\n",
+    cli_rd_cmd_handler,
+    1
+};
+
+/**
+ * Defines the write-address command by its signature, description, callback function pointer,
+ * and number of parameters
+ */
+static const CLI_Command_Definition_t wr_addr_cmd = {
+    "wr",
+    "wr <addr> <val>: Writes the value <val> to the address <addr>\r\n",
+    cli_wr_cmd_handler,
+    2
+};
+
+
+QueueHandle_t cli_rx_queue; //<! Received characters are posted to this queue via the cli_receive() function
+
+
+/**
+ * Registers the defined commands with the CLI, allowing use.
+ */
+static BaseType_t reg_cli_cmds(void) {
+
+    if (FreeRTOS_CLIRegisterCommand(&rd_addr_cmd) != pdPASS) {
         return pdFAIL;
     }
 
-    if (FreeRTOS_CLIRegisterCommand(wr_addr_cmd_ptr) != pdPASS) {
-        xil_printf("Failed to add the write-command for the CLI.\n");
+    if (FreeRTOS_CLIRegisterCommand(&wr_addr_cmd) != pdPASS) {
         return pdFAIL;
     }
 
     return pdPASS;
 }
 
+
 /**
- * @brief Main function for the CLI task
- *
- * The CLI task blocks on a queue into which data is posted by the UART ISR upon reception of data
- * on the serial interface.
- *
- * @param *p Nothing, but must adhere to the FreeRTOS task-function prototype
+ * Function to be called by user when data for the CLI is received
+ * 
+ * @param data Pointer to the received data
+ * @param len The length of the data received
+ * 
+ * @return The number of chars posted to the CLI character receive queue
  */
-void cli_task(void* p) {
+uint32_t cli_receive(char* data, uint32_t len)
+{
+    BaseType_t ret = pdTRUE;
+    uint32_t wrtn = 0;
 
-    static const u8 newline = '\n';
+    while (len-- && ret == pdTRUE) {
+        #if CLI_INPUT_RECVD_FROM_ISR == 1
+        ret = xQueueSendFromISR(cli_rx_queue, data++, pdFALSE);
+        #else
+        ret = xQueueSend(cli_rx_queue, data++, pdFALSE);
+        #endif
+        if (ret == pdTRUE) {
+            wrtn++;
+        }
+    }
 
-    u8 recvd_char;
-    u8 recv_buf[MAX_INPUT_LENGTH];
-    u8 recv_buf_index = 0;
-    u8 ret_buf[MAX_OUTPUT_LENGTH];
+    return wrtn; 
+}
 
-    BaseType_t more_data = pdTRUE;
+/**
+ * Main function for the CLI task. It initializes a reception-queue, registers CLI commands, afterwards
+ * it blocks on the queue. Upon reception of a complete command, the appropriate handler is called
+ * 
+ * The interface responsible for RX/TX of CLI characters must be initialized before calling this function
+ *
+ * @param *p Nothing
+ */
+void cli_task(void *p)
+{
+    char recvd_char;
 
-    /*Replace this with your own UART-/socket initialization procedure, or use
-    one of the two provided examples (UART16550/ARM UART) */
-    uart_init(); 
+    char rx_buf[MAX_INPUT_LENGTH];
+    uint32_t rx_buf_index = 0;
 
-    uart_buf = xQueueCreate(UART_BUF_SIZE, sizeof(u8));
-    if (uart_buf != NULL) {
-        xil_printf("UART stream-buffer initialized.\n");
-    } else {
-        xil_printf("Failed to initialize the UART stream-buffer.\n");
+    #if configAPPLICATION_PROVIDES_cOutputBuffer == 0
+    char *tx_buf = FreeRTOS_CLIGetOutputBuffer();
+    #else
+    char *tx_buf = cli_tx_buf;
+    #endif
+
+    if (tx_buf == NULL) {
+        puts("Failed to get CLI output buffer via FreeRTOS_CLIGetOutputBuffer()");
+        vTaskDelete(NULL);
+    }
+
+
+    BaseType_t is_more = pdTRUE;
+
+    cli_rx_queue = xQueueCreate(CLI_RECV_QUEUE_LEN, sizeof(uint8_t));
+    if (cli_rx_queue == NULL) {
+        puts("Failed to initialize the CLI queue.");
         vTaskDelete(NULL);
     }
 
     if (reg_cli_cmds() == pdPASS) {
-        xil_printf("CLI task initialized. Type \"help\" to see a list of available commands.\n");
+        puts("CLI task initialized. Type \"help\" to see a list of available commands.");
     } else {
-        xil_printf("Failed to initialize the CLI task.\n");
+        puts("Failed to initialize the CLI task.");
         vTaskDelete(NULL);
     }
 
-    xQueueReset(uart_buf);
+    xQueueReset(cli_rx_queue);
 
     while (1) {
-        xQueueReceive(uart_buf, &recvd_char, portMAX_DELAY); //Could get more than 1 and loop but w/e
+        xQueueReceive(cli_rx_queue, &recvd_char, portMAX_DELAY);
 
-        if (recvd_char == '\n') { //Received newline-char implies complete command received. Call command interpreter.
+        if (recvd_char == '\n') { //Complete command received
             do {
-                more_data = FreeRTOS_CLIProcessCommand((char*) recv_buf, (char*) ret_buf, MAX_OUTPUT_LENGTH);
+                is_more = FreeRTOS_CLIProcessCommand(rx_buf, tx_buf, MAX_OUTPUT_LENGTH);
 
-                u16 len_to_send = strlen((char*) ret_buf); //Any output now stored in ret_buf, so send it.
-                if (len_to_send > 0) {
-                    /*Replace this with your own UART-/socket send-procedure, or use
-                    one of the two provided examples (UART16550/ARM UART) */
-                    uart_send(ret_buf, strlen((char*) ret_buf));
-                    uart_send(&newline, 1);
+                uint16_t tx_len = strlen(tx_buf);
+                if (tx_len > 0) {
+                    rx_buf[rx_buf_index] = '\n';
+                    cli_send(rx_buf, strlen(tx_buf));
                 }
 
-            } while (more_data == pdTRUE);
+            } while (is_more == pdTRUE);
 
-            //Done with command, so send data, reset index and buffers.
-            recv_buf_index = 0;
-            memset(recv_buf, 0x00, MAX_INPUT_LENGTH);
-            memset(ret_buf, 0x00, MAX_OUTPUT_LENGTH);
+            rx_buf_index = 0;
+            memset(rx_buf, 0x00, MAX_INPUT_LENGTH);
+            memset(tx_buf, 0x00, MAX_OUTPUT_LENGTH);
 
         } else { //Not done with command
-            if (recvd_char == '\r') {
-                //Don't care about carriage return
-            } else if (recvd_char == '\b') { //Backspace. Remove last char in buffer, if any exist, and zero terminate new resulting string
-                if (recv_buf_index > 0) {
-                    recv_buf[--recv_buf_index] = '\0';
+            if (recvd_char == '\b') { //Backspace
+                if (rx_buf_index > 0) {
+                    rx_buf[--rx_buf_index] = '\0';
                 }
-            } else { //Add received character to receive buffer
-                if (recv_buf_index < MAX_INPUT_LENGTH) {
-                    recv_buf[recv_buf_index++] = recvd_char;
-                } //Should handle the edge case where we receive data in excess of our max buffer size.
+            } else if (recvd_char != '\r') { //If not carriage return, then append the received character to buffer
+                if (rx_buf_index < MAX_INPUT_LENGTH) {
+                    rx_buf[rx_buf_index++] = recvd_char;
+                }
             }
         }
 
@@ -127,43 +184,42 @@ void cli_task(void* p) {
 }
 
 /**
- * @brief Implements the read-address command for the CLI.
+ * Implements the read-address command for the CLI.
  *
- * @param *wr_buf A buffer into which the reply (in this case the value at the provided address) is written
+ * @param *wr_buf A buffer into which the reply is written
  * @param wr_buf_len The size of the buffer pointed to by *wr_buf
  * @param *cmd_str A string containing the received command
  *
  * @return pdTRUE if a partial reply was placed into wr_buf and the function should be called again.
  *         pdFALSE if the complete reply was placed into wr_buf
  */
-BaseType_t rd_addr_cli(s8 *wr_buf, size_t wr_buf_len, const s8 *cmd_str) {
-    u32 addr, val;
+static BaseType_t cli_rd_cmd_handler(char *wr_buf, size_t wr_buf_len, const char *cmd_str)
+{
+    uint32_t addr, val;
     const char *addr_param;
-    u8 **param_err_ptr = NULL;
     BaseType_t addr_param_len;
 
     if (wr_buf_len < 5) { //4 bytes for value, 1 byte for zero termination
-        xil_printf("rd_addr_cli(): Insufficient buffer size. Should be at least 5B but was %uB\n", wr_buf_len);
+        puts("cli_rd_cmd_handler(): Insufficient buffer size. Must be at least 5B");
         return pdFALSE;
     }
 
-    addr_param = FreeRTOS_CLIGetParameter((char*) cmd_str, 1, &addr_param_len);
+    addr_param = FreeRTOS_CLIGetParameter(cmd_str, 1, &addr_param_len);
+    addr = strtoul(addr_param, NULL, 0);
 
-    addr = strtoul((char*) addr_param, (char**) param_err_ptr, 0);
-
-    if (param_err_ptr == NULL) {
-        val = Xil_In32(addr);
+    if (addr != 0) {
+        val = cli_rd_addr(addr);
     } else {
-        val = 0;
+        puts("Error parsing provided address");
+        return pdFALSE;
     }
 
     sprintf((char*) wr_buf, "%lu", val);
-
     return pdFALSE; //Done
 }
 
 /**
- * @brief Implements the write-address command for the CLI.
+ * Implements the write-address command for the CLI.
  *
  * @param *wr_buf A buffer into which the reply (in this case nothing) is written
  * @param wr_buf_len The size of the buffer pointed to by *wr_buf
@@ -176,34 +232,30 @@ BaseType_t rd_addr_cli(s8 *wr_buf, size_t wr_buf_len, const s8 *cmd_str) {
  *       include a buffer and length in order to satisfy the requirements of a CLI command callback function.
  *       Since the buffer is not used, no checks are performed on it.
  */
-BaseType_t wr_addr_cli(s8 *wr_buf, size_t wr_buf_len, const s8 *cmd_str) {
-    u32 addr, val;
+static BaseType_t cli_wr_cmd_handler(char *wr_buf, size_t wr_buf_len, const char *cmd_str)
+{
+    uint32_t addr = 0;
+    uint32_t val = 0;
+
     const char *addr_param, *val_param;
-    u8 **param_err_ptr = NULL;
+    char **str_end_pp = NULL;
     BaseType_t addr_param_len, val_param_len;
 
-    addr_param = FreeRTOS_CLIGetParameter((char*) cmd_str, 1, &addr_param_len);
-    val_param = FreeRTOS_CLIGetParameter((char*) cmd_str, 2, &val_param_len);
+    addr_param = FreeRTOS_CLIGetParameter(cmd_str, 1, &addr_param_len);
+    val_param = FreeRTOS_CLIGetParameter(cmd_str, 2, &val_param_len);
 
-    addr = strtoul((char*) addr_param, (char**) param_err_ptr, 0);
-    val = strtoul((char*) val_param, (char**) param_err_ptr, 0);
+    addr = strtoul(addr_param, str_end_pp, 0);
+    if (addr == 0) {
+        puts("Error parsing provided address");
+        return pdFALSE;
+    }
 
-    if (param_err_ptr == NULL) {
-        Xil_Out32(addr, val);
+    val = strtoul(val_param, str_end_pp, 0);
+    if (val == 0 && (str_end_pp == NULL || *str_end_pp == wr_buf)) {
+        puts("Error parsing provided write-value");
+    } else {
+        cli_wr_addr(addr, val);
     }
 
     return pdFALSE; //Done
-}
-
-
-/**
- * @brief Checks if the input pointed to by data is on the form 0x<hex_chars>.
- *
- * @param *data Pointer to the data string to check
- * @len Length of the data string pointed to by *data
- *
- * @return 1 if string is hex-formatted, 0 otherwise
- */
-u8 is_hex_enc(u8 *data, u8 len) {
-    return (strstr((char*) data, "0x") == (char*) data && (data[strspn((char*) (data + 2), "0123456789abcdefABCDEF")] == 0));
 }
